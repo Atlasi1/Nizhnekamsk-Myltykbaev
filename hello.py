90c27a2651247f9ab335bda543268fabc7d9c68b
Atlasi1 <38984950+Atlasi1@users.noreply.github.com>
2020-11-27 18:18:53 +0300

    Update hello.py
    It's me, maybe

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ def main():
     print("Hello git world")
     print('And I change it every day')
     print("-------------------------")
-
+    print('UFO came and added this line')
 
 if __name__ == "__main__":
     main()
